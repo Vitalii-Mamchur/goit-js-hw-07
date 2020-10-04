bb3eb769fe9510297902dfbbcdc0f4adfa7891f6
Vitalii-Mamchur <65975103+Vitalii-Mamchur@users.noreply.github.com>
2020-10-05 00:29:31 +0300

    пуш 4-ех заданий

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+const listEl = document.querySelector("#categories");
+
+console.log(`В списке ${listEl.children.length} категории.`);
+
+const liItemEl = listEl.querySelectorAll("li.item");
+
+liItemEl.forEach(
+  ({ children }) => (
+    console.log(`Категория: ${children[0].textContent}`),
+    console.log(`Количество элементов:${children[1].children.length}`)
+  )
+);
---
+++ js/task-4.js
@@ -0,0 +1,11 @@
+const btnDecEl = document.querySelector('button[data-action="decrement"]');
+const btnIncEl = document.querySelector('button[data-action="increment"]');
+let outEl = document.querySelector("#value");
+
+const onDecrementBtnClick = () =>
+  (outEl.textContent = Number(outRef.textContent) - 1);
+const onIncrementBtnClick = () =>
+  (outEl.textContent = Number(outRef.textContent) + 1);
+
+btnDecEl.addEventListener("click", onDecrementBtnClick);
+btnIncEl.addEventListener("click", onIncrementBtnClick);
